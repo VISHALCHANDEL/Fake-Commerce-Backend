0ef73ce03c3cfed84b28f160645cf118126274a4
Vishal <vishalchandel0987@gmail.com>
2026-04-08 19:27:11 +0530

    Added Many To Many Relationship b/w Product and Order With @JoinTable

---
+++ src/main/java/com/example/FakeCommerce/schema/Order.java
@@ -0,0 +1,33 @@
+package com.example.FakeCommerce.schema;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "orders")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order extends BaseEntity{
+
+    private OrderStatus status;
+
+    @ManyToMany
+    @JoinTable( // Problem with this is that it will create the table but i cannot add other attributes into that table
+        name = "order_products",
+        joinColumns = @JoinColumn(name = "order_id"),// the Fk belonging to the same schema --Order
+        inverseJoinColumns = @JoinColumn(name = "product_id")// the Fk belonging to the other scheme --Product
+
+    )
+    private List<Product>products;
+}
