19e71854f1430b252a5cbd2b2ed885de33a5668c
Vishal <vishalchandel0987@gmail.com>
2026-05-04 19:27:30 +0530

    Added utils layer to give api response e.g. for category

--- src/main/java/com/example/FakeCommerce/controllers/CategoryController.java
+++ src/main/java/com/example/FakeCommerce/controllers/CategoryController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.FakeCommerce.dtos.CreateCategoryRequestDto;
 import com.example.FakeCommerce.schema.Category;
 import com.example.FakeCommerce.services.CategoryService;
+import com.example.FakeCommerce.utils.ApiResponse;
 
 import lombok.RequiredArgsConstructor;
 
@@ -25,9 +26,10 @@ public class CategoryController {
     private final CategoryService categoryService;
 
     @PostMapping
-    public ResponseEntity<Category> createCategory(@RequestBody CreateCategoryRequestDto requestDto){
+    public ResponseEntity<ApiResponse<Category>> createCategory(@RequestBody CreateCategoryRequestDto requestDto){
+         Category category = categoryService.createCategory(requestDto);
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.createCategory(requestDto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(ApiResponse.success(category,"Category created successfully"));
     }
 
     @GetMapping
